9b0f08c26d53a49d9494387b0198d4495643cb03
jonasbn <jonasbn@logicLAB.dk>
2014-02-27 16:27:27 +0100

    Added POD, Changes file and change file test

--- lib/Perl/Critic/Policy/logicLAB/RequireParamsValidate.pm
+++ lib/Perl/Critic/Policy/logicLAB/RequireParamsValidate.pm
@@ -76,10 +76,148 @@ __END__
 
 Perl::Critic::Policy::logicLAB::RequireParamsValidate - simple policy for enforcing use of Params::Validate
 
-=head1 AFFILIATION 
+=head1 AFFILIATION
 
 This policy is a policy in the Perl::Critic::logicLAB distribution. The policy
 is themed: logiclab.
 
-=cut
+=head1 VERSION
 
+This documentation describes version 0.01.
+
+=head1 SYNOPSIS
+
+Your code:
+
+    package This::Is::A::Test;
+
+        # code goes here
+
+    1;
+
+Invocation of policy:
+
+    $ perlcritic --single-policy logicLAB::RequirePackageNamePattern lib
+
+Explanation:
+
+    Use specified requirement for package naming for This::Is::A::Test
+
+Description:
+
+    Package name: This::Is::A::Test is not complying with required standard
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+No special requirements or environemtn required.
+
+=head1 DEPENDENCIES AND REQUIREMENTS
+
+=over
+
+=item * L<Module::Build>
+
+=item * L<Perl::Critic>
+
+=item * L<Perl::Critic::Utils>
+
+=item * L<Perl::Critic::Policy>
+
+=item * L<Test::More>
+
+=item * L<Test::Class>
+
+=item * L<Test::Perl::Critic>
+
+=item * L<Data::Dumper>
+
+=item * L<File::Spec>
+
+=item * L<List::MoreUtils>
+
+=item * L<Params::Validate>
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+This distribution has no known incompatibilities.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs or limitations
+
+=head1 TEST AND QUALITY
+
+The following policies have been disabled for this distribution
+
+=over
+
+=item * L<Perl::Critic::Policy::ValuesAndExpressions::ProhibitConstantPragma>
+
+Constants are good, - see the link below.
+
+=over
+
+=item * L<https://logiclab.jira.com/wiki/display/OPEN/Perl-Critic-Policy-ValuesAndExpressions-ProhibitConstantPragma>
+
+=back
+
+=item * L<Perl::Critic::Policy::NamingConventions::Capitalization>
+
+=back
+
+See also F<t/perlcriticrc>
+
+=head2 TEST COVERAGE
+
+Coverage test executed the following way, the coverage report is based on the
+version described in this documentation (see L</VERSION>).
+
+    ./Build testcover
+
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+    File                           stmt   bran   cond    sub    pod   time  total
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+    .../RequireParamsValidate.pm  100.0  100.0   66.7  100.0  100.0  100.0   98.6
+    Total                         100.0  100.0   66.7  100.0  100.0  100.0   98.6
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Perl::Critic>
+
+=item * L<https://metacpan.org/pod/Params::Validate>
+
+=back
+
+=head1 AUTHOR
+
+=over
+
+=item * Jonas B. Nielsen, jonasbn C<< <jonasbn@cpan.org> >>
+
+=back
+
+=head1 ACKNOWLEDGEMENT
+
+=over
+
+=item * Jeffrey Ryan Thalhammer (THALJEF) and the Perl::Critic contributors for
+Perl::Critic
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2013-2014 Jonas B. Nielsen, jonasbn. All rights reserved.
+
+Perl::Critic::Policy::logicLAB::RequirePackageNamePattern;  is released under
+the Artistic License 2.0
+
+The distribution is licensed under the Artistic License 2.0, as specified by
+the license file included in this distribution.
+
+=cut
